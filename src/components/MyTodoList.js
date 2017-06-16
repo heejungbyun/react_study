961253c65b9255f166eb97346f10c84ae48ed4b1
heejungbyun <vvvvvby@hotmail.com>
2017-06-16 18:39:43 +0900

    하나로 합침

--- src/components/MyTodoList.js
+++ src/components/MyTodoList.js
@@ -3,47 +3,39 @@ import ReactDOM from 'react-dom';
 
 // List 컴포넌트
 class TodoList extends React.Component{
-    render(){
-        return(
-            <div>
-                <TodoNote/>
-                <TodoForm/>
-            </div>
-
-        )
-    }
-}
-
-// 실질적인 List 컴포넌트
-class TodoNote extends React.Component{
-    render(){
-        return(
-            <ul className="list_todo">
-                <li>
-                    <label>라벨</label>
-                    <input type="checkbox"/>
-                    <strong>내용</strong>
-                    <span className="date">날짜</span>
-                    <button className="btn_modify">수정</button>
-                    <button className="btn_del">삭제</button>
-                </li>
-            </ul>
-        )
-    }
-}
-
-//입력 컴포넌트
-class TodoForm extends React.Component{
+	constructor(props){
+		super(props);
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.state = {items:[],text:''};
+	}
 	handleChange(e){
-		var text = e.target.value;
-		return text
+
+	}
+	handleSubmit(e){
+
 	}
     render(){
         return(
-            <div className="form_todo" onSubmit={this._handleSubmit}>
-                <input type="text" onChange={this.handleChange}/>
-                <button>입력</button>
+            <div>
+				<ul className="list_todo">
+					<li>
+						<label>라벨</label>
+						<input type="checkbox"/>
+						<strong>내용</strong>
+						<span className="date">날짜</span>
+						<button className="btn_modify">수정</button>
+						<button className="btn_del">삭제</button>
+					</li>
+				</ul>
+				<div className="form_todo">
+					<form onSubmit={this.handleSubmit}>
+		                <input type="text" onChange={this.handleChange} value={this.state.text}/>
+		                <button>{'입력' + (this.state.items.length +1)}</button>
+					</form>
+	            </div>
             </div>
+
         )
     }
 }
