1a468b3c02e2b7db0ce11feaa30fe44d67926307
heejungbyun <vvvvvby@hotmail.com>
2017-06-24 15:35:02 +0900

    React 날짜 추가 Todo:localstorage

--- src/components/MyTodoList.js
+++ src/components/MyTodoList.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//localstorage -> 2.5mb ~ 5mb 저장가능 html5 에서 지원, localstorage
 // List 컴포넌트
 class TodoList extends React.Component{
 	constructor(props){
 		super(props);
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
-		this.state = {items:[],text:''};
+		this.state = {items:[],text:'',date:''};
 	}
 
 	handleChange(e){
@@ -16,13 +17,24 @@ class TodoList extends React.Component{
 
 	handleSubmit(e){
 		e.preventDefault();
+		//날짜
+		function getCurrentDate(){
+			var dateObj = new Date();
+			var year = dateObj.getFullYear();
+			var month = dateObj.getMonth()+1;
+			var day = dateObj.getDate();
+
+			return year+'/'+month+'/'+day;
+		}
 		var newItem = {
 			text: this.state.text,
-			id:Date.now()
+			id:Date.now(),
+			date: getCurrentDate()
 		};
 		this.setState((prevState) => ({
 			items: prevState.items.concat(newItem),
-			text: ''
+			text: '',
+			date: '',
 		}))
 
 	}
@@ -31,13 +43,22 @@ class TodoList extends React.Component{
             <div>
 				<ul>
 				  {this.state.items.map(item => (
-					<li key={item.id}>{item.text}</li>
+					<li key={item.id} className="list_item">
+					<label htmlFor="chk">했음</label>
+					<input type="checkbox" id="chk"/>
+						{item.text}
+					<div className="other_info">
+						<span className="date">{item.date}</span>
+						<button className="btn_modify">수정</button>
+						<button className="btn_del">삭제</button>
+					</div>
+					</li>
 				  ))}
 				</ul>
 				<div className="form_todo">
 					<form onSubmit={this.handleSubmit}>
 		                <input type="text" onChange={this.handleChange} value={this.state.text}/>
-		                <button>{'입력' + (this.state.items.length +1)}</button>
+		                <button>입력</button>
 					</form>
 	            </div>
             </div>
